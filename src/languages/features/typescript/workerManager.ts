fdee81fba134db0da633548bbc8046d3f05bda84
Kleis Auke Wolthuizen <github@kleisauke.nl>
2026-01-13 19:48:10 +0100

    Add magic webpack comments

--- src/languages/features/typescript/workerManager.ts
+++ src/languages/features/typescript/workerManager.ts
@@ -62,7 +62,7 @@ export class WorkerManager {
 				this._worker = createWebWorker<TypeScriptWorker>({
 					// module that exports the create() method and returns a `TypeScriptWorker` instance
 					moduleId: 'vs/language/typescript/tsWorker',
-					createWorker: () => new Worker(new URL('./ts.worker.js', import.meta.url), { type: 'module' }),
+					createWorker: () => new Worker(new URL('./ts.worker.js', import.meta.url) /* webpackChunkName: "monaco-ts.worker" */, { type: 'module' }),
 
 					label: this._modeId,
 
