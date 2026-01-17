1d1f0f9c06c7694cbdf73adc997b0f044e79bea6
Kleis Auke Wolthuizen <github@kleisauke.nl>
2026-01-17 10:40:53 +0100

    Revert "Add magic webpack comments"
    This reverts commit fdee81fba134db0da633548bbc8046d3f05bda84.

--- src/languages/features/typescript/workerManager.ts
+++ src/languages/features/typescript/workerManager.ts
@@ -62,10 +62,7 @@ export class WorkerManager {
 				this._worker = createWebWorker<TypeScriptWorker>({
 					// module that exports the create() method and returns a `TypeScriptWorker` instance
 					moduleId: 'vs/language/typescript/tsWorker',
-					createWorker: () => new Worker(
-						new URL('./ts.worker.ts?esm', import.meta.url), // webpackChunkName: "monaco-ts.worker"
-						{ type: 'module' }
-					),
+					createWorker: () => new Worker(new URL('./ts.worker?esm', import.meta.url), { type: 'module' }),
 
 					label: this._modeId,
 
